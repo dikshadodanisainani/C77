ba05019195a391e009aa8b9a1f38755e5e353b50
dikshadodanisainani <54570385+dikshadodanisainani@users.noreply.github.com>
2020-07-25 20:47:13 +0530

    Add files via upload

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View ,TextInput, Alert, TouchableOpacity} from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
+import SantaAnimation from '../components/SantaClaus';
+export default class WelcomeScreen extends Component
+{
+    constructor(){
+        super();
+        this.state={
+            emailId:'',
+            password:''
+        }
+    }
+
+    userSignUp=(emailId,password)=>{
+        firebase.auth().createUserWithEmailAndPassword(emailId,password).then((response)=>{
+            return Alert.alert("User Added");
+        })
+        .catch(function(error){
+            var errorCode=error.code;
+            var errorMessage=error.message;
+            return Alert.alert(errorMessage);
+        })
+    }
+    userLogin=(emailId,password)=>{
+        firebase.auth().signInWithEmailAndPassword(emailId,password).then(()=>{
+            return Alert.alert("Successfully Login");
+        })
+        .catch((error)=>{
+            var errorCode=error.code;
+            var errorMessage=error.message;
+            return Alert.alert(errorMessage);
+        })
+    }
+    render()
+    {
+        return(
+            <View style={styles.container}>
+                <View >
+                   
+                    <Text style={styles.title}>Book Santa</Text>
+                </View>
+                <View>
+                <TextInput
+                style={styles.inputBox}
+                placeholder="abc@example/com"
+                keyboardType='email-address'
+                onChangeText={(text)=>{
+                    this.setState({
+                        emailId:text
+                    })
+                }}/>
+                 <TextInput
+                style={styles.inputBox}
+                placeholder="EnterPassword"
+               secureTextEntry={true}
+                onChangeText={(text)=>{
+                    this.setState({
+                        password:text
+                    })
+                }}/>
+                <TouchableOpacity style={[styles.button,{marginBottom:20,marginTop:20}]}
+                onPress={()=>{this.userLogin(this.state.emailId,this.state.password)}}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button}
+                onPress={()=>{this.userSignUp(this.state.emailId,this.state.password)}}>
+                    <Text style={styles.buttonText}>SignUp</Text>
+                </TouchableOpacity>
+                </View>
+               
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      backgroundColor:'#F8BE85',
+      justifyContent:'center',
+      alignItems:'center',
+    },
+    profileContainer:{
+      flex:1,
+     
+     
+    },
+    title :{
+      fontSize:65,
+      fontWeight:'300',
+      paddingBottom:30,
+      color : '#ff3d00'
+    },
+    inputBox:{
+      width: 300,
+      height: 40,
+      borderBottomWidth: 1.5,
+      borderColor : '#ff8a65',
+      fontSize: 20,
+      margin:10,
+      paddingLeft:10,
+      
+    },
+    button:{
+      width:300,
+      height:50,
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:25,
+      backgroundColor:"#ff9800",
+      shadowColor: "#000",
+      shadowOffset: {
+         width: 0,
+         height: 8,
+      },
+      shadowOpacity: 0.30,
+      shadowRadius: 10.32,
+      elevation: 16,
+    },
+    buttonText:{
+      color:'#ffff',
+      fontWeight:'200',
+      fontSize:20
+    },
+    buttonContainer:{
+      flex:1,
+      alignItems:'center'
+    }
+  })
